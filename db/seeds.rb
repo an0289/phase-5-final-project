29e1c25e2c5cba6e3e272bbdafd96e1907f938dc
Alexandra <alexandraneff@live.com>
2023-08-21 10:33:54 -0500

    react-router-dom added and working

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,13 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+# Events: thinking of doing something blood borne related events, or justs eldritch horror related events 
+#  - Amygdala location at the Cathedral ward to witness the Hunter's Nightmare  
+#  - Miclosh, host of the Nightmare 
+#  - Rom, at Moonside Lake 
+#  - Kos at Fishing Hamlet 
+#  - Ludwig at the Nightmare Church (originally at the Healing Church and lead hunters)
+# Don't have to do any reviews, but can add them if I want, will just have to create a rule that a review cannot be left if the event date is later than when the review is being left I guess 
+# Tickets will be a bit easier since they do not have any components to them 
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
