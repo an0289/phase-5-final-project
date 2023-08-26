0792ea52515406f52bfe08a18652f4ca7bd4d6cb
Alexandra <alexandraneff@live.com>
2023-08-26 09:25:40 -0500

    added routes to app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { UserContext } from '../contexts/UserContext'
+import NavBar from './NavBar'
+import Login from '../pages/Login'
+import EventList from '../pages/EventList'
+import MyEventList from '../pages/MyEventList'
+import NewEvent from '../pages/NewEvent'
+
 
 function App() {
-  
+  const {user} = useContext(UserContext)
+
+  // if (!user) return <Login />
+
+  return (
+    <>
+    <main>
+      <Routes>
+        <Route path="/new"
+          element={<NewEvent />}
+        />
+        <Route path="/"
+          element={<EventList />}
+        />
+        <Route path="/my_events"
+          element={<MyEventList />}
+        />
+      </Routes>
+    </main>
+    </>
+  )
+
 }
 
 export default App;
---
+++ client/src/pages/MyEventList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function MyEventList() {
+
+}
+
+export default MyEventList 
