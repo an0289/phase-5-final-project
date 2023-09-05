4ab94a9d4d84e1f5c070b6a1c4da296a5fac6c7f
Alexandra <alexandraneff@live.com>
2023-09-05 12:28:07 -0500

    removed case statement in users_controllers since it was unnecessary

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,15 @@ class UsersController < ApplicationController
     end 
 
     def create
-        case params[:type]
-        when 'Attendee'
-            @user = Attendee.create!(user_params)
-            UserMailer.with(user: @user).new_user_email.deliver_later
-        when 'Organizer'
-            @user = Organizer.create!(user_params)
-            UserMailer.with(user: @user).new_user_email.deliver_later
-        end 
+        # case params[:type]
+        # when (params[:type] === 'Attendee')
+        #     @user = Attendee.create!(user_params)
+        #     UserMailer.with(user: @user).new_user_email.deliver_later
+        # when (params[:type] === 'Organizer')
+        #     @user = Organizer.create!(user_params)
+        #     UserMailer.with(user: @user).new_user_email.deliver_later
+        # end 
+        @user = User.create!(user_params)
         session[:user_id] = @user.id
         render json: @user, status: :created 
     end 
